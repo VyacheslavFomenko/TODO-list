86a4444cc620b5155dee8809302b44dbdaeb9fbb
Pobedilda <playfog3@gmail.com>
2024-09-04 14:45:09 +0300

    add: models for User, Task, Status, FileImage and migrations for them

--- task/models.py
+++ task/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from user.models import User
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=255)
+    editable = models.BooleanField(default=False)
+
+
+class FileImage(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(db_index=True)
+    doc = models.ImageField(upload_to='uploads/')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name="task")
+    assigned_to = models.ManyToManyField(User, related_name="task")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
