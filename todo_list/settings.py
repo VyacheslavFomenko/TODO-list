42b946015ab8a507f35486fa4de6d80b1b350e5c
Pobedilda <playfog3@gmail.com>
2024-09-05 13:51:12 +0300

    add: jwt token authentication, register, manage end points

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -104,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
 AUTH_USER_MODEL = "user.User"
 
 # Internationalization
