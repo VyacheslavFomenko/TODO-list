254f7a046b656d25c953b598f216c55b1295551c
Pobedilda <playfog3@gmail.com>
2024-09-06 12:51:15 +0300

    refactor: change view for Status and Task serializers

--- task/serializers.py
+++ task/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
 from task.models import FileImage, Task, Status
 from django.contrib.contenttypes.models import ContentType
+
+from user.serializers import UserDetailSerializer
 
 
 class FileImageSerializer(serializers.ModelSerializer):
@@ -10,19 +13,27 @@ class FileImageSerializer(serializers.ModelSerializer):
         fields = ("id", "content_type", "object_id", "doc")
 
 
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ("id", "name", "editable")
+
+
 class TaskSerializer(serializers.ModelSerializer):
     image_files = serializers.SerializerMethodField()
+    status_detailed = StatusSerializer(source="status", read_only=True)
+    assigned_to_detailed = UserDetailSerializer(
+        many=True,
+        source="assigned_to"
+    )
 
     class Meta:
         model = Task
-        fields = ("id", "title", "description", "status", "assigned_to", "created_at", "updated_at", "image_files")
+        fields = (
+            "id", "title", "description", "status", "status_detailed", "assigned_to", "assigned_to_detailed",
+            "created_at", "updated_at", "image_files")
 
     def get_image_files(self, obj):
         qs = FileImage.objects.filter(object_id=obj.id, content_type=ContentType.objects.get_for_model(Task))
         return FileImageSerializer(qs, many=True).data
 
-
-class StatusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Status
-        fields = ("id", "name")
