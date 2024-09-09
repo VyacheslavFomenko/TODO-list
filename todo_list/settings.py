2f51cd969d9ef5398fd2f6d1161106c08acf4e98
Pobedilda <playfog3@gmail.com>
2024-09-09 14:54:51 +0300

    refactor: remove timezone for celery

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -137,8 +137,6 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -149,8 +147,6 @@ CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND")
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Europe/kyiv'
-
 
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
