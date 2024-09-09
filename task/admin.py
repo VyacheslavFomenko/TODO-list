8c8e188957c38aba902904d6420b65183fcd56ac
Pobedilda <playfog3@gmail.com>
2024-09-09 14:52:19 +0300

    add; model to admin panel

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from task.models import Task, Status, FileImage
+
 # Register your models here.
+
+admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(FileImage)
